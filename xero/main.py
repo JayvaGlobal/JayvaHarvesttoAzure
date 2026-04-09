5ea5f5fbc64a11f37d8aead32a548188338509c8
JayvaGlobal <aneta.kasprzak@jayvaglobal.com>
2026-04-09 16:22:49 +1000

    Update main.py

--- xero/main.py
+++ xero/main.py
@@ -1,6 +1,11 @@
+from .config import XERO_CLIENT_ID, XERO_CLIENT_SECRET, XERO_REFRESH_TOKEN
 from .auth import refresh_access_token, get_connections
 
 def main():
+    print("CLIENT ID loaded:", bool(XERO_CLIENT_ID))
+    print("CLIENT SECRET loaded:", bool(XERO_CLIENT_SECRET))
+    print("REFRESH TOKEN loaded:", bool(XERO_REFRESH_TOKEN))
+
     token_data = refresh_access_token()
     access_token = token_data["access_token"]
 
